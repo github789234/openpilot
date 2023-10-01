300284a307e3fb8622b9783599e3d6c1d4f47d7c
vbifhs <100491926+vbifhs@users.noreply.github.com>
2023-10-01 15:32:55 -0700

    fixed ref to BODY_CONTROL_STATE_2

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -140,7 +140,7 @@ class CarState(CarStateBase):
 
     # UI_SET_SPEED is always non-zero when main is on, hide until first enable
     if ret.cruiseState.speed != 0:
-      is_metric = cp_cam.vl["_2"]["UNITS"] in (1, 2)
+      is_metric = cp_cam.vl["BODY_CONTROL_STATE_2"]["UNITS"] in (1, 2)
       conversion_factor = CV.KPH_TO_MS if is_metric else CV.MPH_TO_MS
       ret.cruiseState.speedCluster = cluster_set_speed * conversion_factor
 
