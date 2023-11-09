ccc91fd1719afcd90c8b0ca89b7a0928bfb222d9
vbifhs <100491926+vbifhs@users.noreply.github.com>
2023-11-09 06:48:19 -0800

    changed can bus numbering back to use internal panda

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -230,7 +230,7 @@ class CarState(CarStateBase):
     #     ("PRE_COLLISION", 33),
     #   ]
 
-    return CANParser(DBC[CP.carFingerprint]["pt"], messages, 4)  
+    return CANParser(DBC[CP.carFingerprint]["pt"], messages, 0)  
 
   @staticmethod
   def get_cam_can_parser(CP):
@@ -259,4 +259,4 @@ class CarState(CarStateBase):
     #     ("PCS_HUD", 1),
     #   ]
 
-    return CANParser(DBC[CP.carFingerprint]["pt"], messages, 5)
+    return CANParser(DBC[CP.carFingerprint]["pt"], messages, 1)
